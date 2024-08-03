3d32fa3b0aa7093b6faba451c2c9ff86fc581a0d
Luis2022cr <joseluiscrz129@gmail.com>
2024-08-03 01:16:52 -0600

    fixed error

--- src/components/HeaderAlumno.tsx
+++ src/components/HeaderAlumno.tsx
@@ -23,7 +23,7 @@ const Header: React.FC = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className='ml-auto'>
-        <Nav.Link className='nav-link-custom' as={Link} to="/gestion_alumno">
+          <Nav.Link className='nav-link-custom' as={Link} to="/gestion_alumno">
             Inicio
           </Nav.Link>
           <Nav.Link className='nav-link-custom' as={Link} to="/actividades/crear">
